3e1d796cc4b72c6009d38a81088fc5538d008040
hanjh13-cloud <hanjh13@gmail.com>
2026-08-11 15:30:59 +0900

    Create News_bot.py

---
+++ News_bot.py
@@ -0,0 +1,63 @@
+import asyncio
+import feedparser
+from telegram import bot
+
+telegram_token = 8807684716:AAE_lZ7HSIXNiM70O27nDOfzhUhcGSu-NXY
+chat_id = 614352234
+
+RSS_SOURCE = [
+  "http://news.google.com/rss/search?q=산업재해&h1=ko&g1=KR&ceid=KR:ko"
+  "http://news.google.com/rss/search?q=중대재해&h1=ko&g1=KR&ceid=KR:ko"
+  "http://news.google.com/rss/search?q=산업안전+사고&h1=ko&g1=KR&ceid=KR:ko"
+  "http://news.google.com/rss/search?q=산업보건+재해&h1=ko&g1=KR&ceid=KR:ko"
+  "http://news.google.com/rss/search?q=건설현장+사고&h1=ko&g1=KR&ceid=KR:ko"
+  "http://news.google.com/rss/search?q=산업안전보건법&h1=ko&g1=KR&ceid=KR:ko"
+  "http://news.google.com/rss/search?q=위험물안전관리법&h1=ko&g1=KR&ceid=KR:ko"
+  "http://news.google.com/rss/search?q=소방시설법&h1=ko&g1=KR&ceid=KR:ko"
+]
+
+KEYWORD_WHITELIST = ["산업재해", "중대재해", "산업안전", "산업보건", "안전사고", "추락", "끼임", "질식", "폭발", "화재사고", "산재", "근로자 사망", "작업중 사망"]
+KEYWORD_BLACKLIST = ["안전자산", "안전마진"]
+SENT_LOG_FILE = "sent_news.bot"
+
+def load_sent_log():
+  try:
+    with open(sent_log_file, "r", encording="utf-8") as f:
+      return set(line.strip() for line in f.readlines())
+  except FileNotFoundError:
+    return set()
+
+def save_sent_log(sent_set):
+  with open(SENT_LOG_FILE, "w", encoding="utf-8") as f:
+    for link in sent_set:
+      f.write(link+"\n")
+
+def keyword_filter(title):
+  if any(bad in title for bad in KEYWORD_BLACKLIST):
+    return False
+  return any(good in title for good in KEYWORD_WHITELIST)
+
+async def send_news():
+  bot = Bot(token=TELEGRAM_TOKEN)
+  sent_log = load_sent_log()
+  new_sent = set(sent_log)
+
+for rss_url in RSS_SOURCES:
+  feed = feedparser.parse(rss_url)
+  for entry in feed.entriws[:10]:
+    title = entry.title
+    link = entry.link
+    
+    if link in sent_log:
+      continue
+    if not keyword_filter(title):
+      continue
+      
+    message = f"산업안전/재해 뉴스\n\n{title}\n\n {link}"
+    await bot.send_message(chat_id=CHAT_ID, text=message)
+    print(f"전송완료": {title}")
+    new_sent.add(link)
+save_sent_log(new_sent)
+
+if __name__ == "__main__":
+  asyncio.run(send_news())
